785a9b30f1d6ea0ca864494cd3be95850301414a
alin.rautoiu <alin.rautoiu@teamnet.ro>
2016-08-11 14:02:14 +0300

    Some refactorization. Episode selection screen. Added EventBus. Scale up/down events.

--- app/src/main/java/com/example/alinrautoiu/webcomicrclient/main/comic/ComicBookDisplayActivity.java
+++ app/src/main/java/com/example/alinrautoiu/webcomicrclient/main/comic/ComicBookDisplayActivity.java
@@ -2,15 +2,20 @@ package com.example.alinrautoiu.webcomicrclient.main.comic;
 
 import android.content.Context;
 import android.content.Intent;
+
 import android.os.Bundle;
+
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.OrientationHelper;
 import android.support.v7.widget.RecyclerView;
+import android.view.MotionEvent;
+import android.view.ScaleGestureDetector;
+import android.view.View;
+import android.widget.FrameLayout;
 
 import com.example.alinrautoiu.webcomicrclient.R;
 import com.example.alinrautoiu.webcomicrclient.common.Episode;
-import com.example.alinrautoiu.webcomicrclient.common.Series;
 import com.example.alinrautoiu.webcomicrclient.utils.Utils;
 
 import butterknife.BindView;
@@ -20,11 +25,59 @@ import butterknife.ButterKnife;
 
 public class ComicBookDisplayActivity extends AppCompatActivity {
 
+    private static final int MIN_COLUMN_COUNT = 1;
+    private static final int MAX_COLUMN_COUNT = 4;
     @BindView(R.id.comic_rv) RecyclerView comicRecyclerView;
-    private ComicAdapter comicAdapter;
-    private ComicBookDisplayPresenter presenter;
-    private Context context;
+    @BindView(R.id.activity_comic_book_display) FrameLayout layout;
+
+    ScaleGestureDetector scaleGestureDetector;
+    private float firstSpanX;
+    private float firstSpanY;
+
+    //TODO Find a more correct way to get scale up/down events
+    ScaleGestureDetector.OnScaleGestureListener scaleGestureListener = new ScaleGestureDetector.OnScaleGestureListener() {
+        @Override
+        public boolean onScale(ScaleGestureDetector scaleGestureDetector) {
+            return true;
+        }
+
+        @Override
+        public boolean onScaleBegin(ScaleGestureDetector scaleGestureDetector) {
+            firstSpanX = scaleGestureDetector.
+                    getCurrentSpanX() * scaleGestureDetector.getScaleFactor();
+            firstSpanY = scaleGestureDetector.
+                    getCurrentSpanY() * scaleGestureDetector.getScaleFactor();
+
+            return true;
+        }
+
+        @Override
+        public void onScaleEnd(ScaleGestureDetector scaleGestureDetector) {
+            float currentSpanX = scaleGestureDetector.getCurrentSpanX()
+                    * scaleGestureDetector.getScaleFactor();
+            float currentSpanY = scaleGestureDetector.getCurrentSpanY()
+                    * scaleGestureDetector.getScaleFactor();
+            if (currentSpanX > firstSpanX || currentSpanY > firstSpanY) {
+                columnCount = columnCount - 1 == 0 ? MIN_COLUMN_COUNT : columnCount - 1;
+                updateColumnLayout();
+            } else if (currentSpanX < firstSpanX || currentSpanY < firstSpanY) {
+                columnCount = columnCount + 1 == 5 ? MAX_COLUMN_COUNT : columnCount + 1;
+                updateColumnLayout();
+            }
+        }
+    };
+
+    private void updateColumnLayout() {
+        comicRecyclerView.setLayoutManager(new GridLayoutManager(context, columnCount));
+    }
+
+    ComicAdapter comicAdapter;
+    ComicBookDisplayPresenter presenter;
+    Context context;
+
     int episodeId;
+    int columnCount;
+
     public static Intent getStartIntent(Context context) {
         return new Intent(context, ComicBookDisplayActivity.class);
     }
@@ -43,6 +96,15 @@ public class ComicBookDisplayActivity extends AppCompatActivity {
         }
         context = this;
 
+        scaleGestureDetector = new ScaleGestureDetector(this, scaleGestureListener);
+        comicRecyclerView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                scaleGestureDetector.onTouchEvent(motionEvent);
+                return false;
+            }
+        });
+
         presenter = new ComicBookDisplayPresenter(this);
         presenter.loadPanels(episodeId);
     }
@@ -51,13 +113,12 @@ public class ComicBookDisplayActivity extends AppCompatActivity {
         int orientation = Utils.getOrientation(context);
 
         if (orientation == OrientationHelper.VERTICAL) {
-            comicRecyclerView.setLayoutManager(
-                    new GridLayoutManager(context, 2));
+            columnCount = 2;
         } else {
-            comicRecyclerView.setLayoutManager(
-                    new GridLayoutManager(context, 4));
+            columnCount = 4;
         }
 
+        updateColumnLayout();
         comicRecyclerView.setAdapter(comicAdapter = new ComicAdapter());
 
         comicAdapter.clear();
